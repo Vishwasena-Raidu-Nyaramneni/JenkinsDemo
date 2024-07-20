97d153da9bf5c5d2afb3dd7f1cb66696146a0bb6
VISHWASENA RAIDU NYARAMNENI <vishwasena.nyaramneni@gmail.com>
2024-07-20 00:43:25 -0500

    added a print statement

--- src/main/java/com/vsr/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/vsr/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Hello Vishwa. This is Initial commit");
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
